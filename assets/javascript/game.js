298a0f88f4a91911e7389c8164ee8b254834700b
Adithya Manohar <manohar.adithya@gmail.com>
2017-11-17 13:59:50 -0500

    3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,7 @@
 
 // Create a word bay to chose things from 
 
-var wordList = ["Batman", "Nightwing", "Robins", "Batgirl", "BatWoman", "Alfred"];
-
-
-
-
-
+var wordList = [ "batman", "batgirl"];
 
 
  $(document).ready(function() {
@@ -20,8 +15,8 @@ var wordList = ["Batman", "Nightwing", "Robins", "Batgirl", "BatWoman", "Alfred"
 
         	// Replace each word with hidden spaces
 			for(var i=0; i < wordList.length; i++) {
- 			// wordList[i] = wordList[i].replace(wordList[i].charAt(0), '-' );
- 			wordList[i] = wordList[i].replace(/[^]/g, "-");
+ 			// wordList[i] = wordList[i].replace(wordList[i].charAt(0), '-' ); - Alternative?
+ 			wordList[i] = wordList[i].replace(/[^ ]/g, "-");
 			}
 
    //      	// Create a method to choose from the array at random	
@@ -30,16 +25,29 @@ var wordList = ["Batman", "Nightwing", "Robins", "Batgirl", "BatWoman", "Alfred"
    //      	// Display the word on the Webpage
 			$("#chosenWordDisplay").text(wordList[s]);
 
+ 			
+        	//When a key is pressed 
+			document.onkeyup = function(event) {
+				
+				//The user guess
+				var userGuess = event.key;
+				// var displayedWord = wordList[s].contains(event.key);
+				
+			// If the guess is correct
+			//var correctGuess = wordList[s].includes(userGuess);
+
+				//If the user key is in string
+				if((wordList[s].includes(userGuess))){
+					alert("Yes");
+				}
+				else {
+					alert("no")
+				}
+
 			
-			
+			}
 
- });
-
-// var str = "Visit Microsoft!";
-// var res = str.replace("Microsoft", "W3Schools");
-
-
-
+});
 
 
  });
